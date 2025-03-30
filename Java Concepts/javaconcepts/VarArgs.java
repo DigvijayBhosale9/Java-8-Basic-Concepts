6c51d590187722af871b9e0237ec9bc70efe426c
Digvijay Bhosale <113753235+DigvijayBhosale9@users.noreply.github.com>
2025-03-30 19:59:32 +0530

    Add files via upload
    javabasics

---
+++ Java Concepts/javaconcepts/VarArgs.java
@@ -0,0 +1,14 @@
+package javaconcepts;
+public class VarArgs {
+  public static void printNames(String name, String... otherNames) {
+    System.out.println("Main name: " + name);
+    for (String otherName : otherNames) {
+      System.out.println("Other name: " + otherName);
+    }
+  }
+
+  public static void main(String[] args) {
+    printNames("Mack");
+    printNames("Rutu", "Jack", "Joy");
+  }
+}
