ef11fdca99bcfa3b5cb887e071ae17196da12cec
Digvijay Bhosale <113753235+DigvijayBhosale9@users.noreply.github.com>
2025-03-30 20:00:08 +0530

    Add files via upload
    Java Keywords

---
+++ Java Keywords/Static/statickeyword/StaticClass.java
@@ -0,0 +1,16 @@
+package statickeyword;
+class MathUtil {
+    //Inner Class
+    public static class Geometry {
+        public static double calculateArea(double radius) {
+            return Math.PI * radius * radius;
+        }
+    }
+}
+
+public class StaticClass {
+    public static void main(String[] args) {
+        double area = MathUtil.Geometry.calculateArea(2);
+        System.out.println("Area: " + area);
+    }
+}
